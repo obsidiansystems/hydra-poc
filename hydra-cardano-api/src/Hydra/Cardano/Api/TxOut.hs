2f74aaa289bb56dcbdbfb4e1687f36448dee86bc
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-02-10 12:27:12 +0100

    fromPlutusAddress now takes in NetworkId instead of Network

--- hydra-cardano-api/src/Hydra/Cardano/Api/TxOut.hs
+++ hydra-cardano-api/src/Hydra/Cardano/Api/TxOut.hs
@@ -12,16 +12,15 @@ import qualified Cardano.Api.UTxO as UTxO
 import qualified Cardano.Ledger.Babbage.TxInfo as Ledger
 import qualified Cardano.Ledger.Core as Ledger
 import qualified Cardano.Ledger.Credential as Ledger
+import qualified Data.List as List
 import Hydra.Cardano.Api.AddressInEra (fromPlutusAddress)
 import Hydra.Cardano.Api.Hash (unsafeScriptDataHashFromBytes)
-import Hydra.Cardano.Api.Network (Network)
 import Hydra.Cardano.Api.ReferenceTxInsScriptsInlineDatumsSupportedInEra (HasInlineDatums, inlineDatumsSupportedInEra)
 import Hydra.Cardano.Api.ScriptData (toScriptData)
 import Hydra.Cardano.Api.ScriptDataSupportedInEra (HasScriptData, scriptDataSupportedInEra)
 import Hydra.Cardano.Api.Value (fromPlutusValue, minUTxOValue)
 import Plutus.V2.Ledger.Api (OutputDatum (..), fromBuiltin)
 import qualified Plutus.V2.Ledger.Api as Plutus
-import qualified Data.List as List
 
 -- * Extras
 
@@ -115,7 +114,7 @@ toLedgerTxOut =
 -- Plutus addresses are stripped off it.
 fromPlutusTxOut ::
   (HasMultiAsset era, HasScriptData era, HasInlineDatums era, IsShelleyBasedEra era) =>
-  Network ->
+  NetworkId ->
   Plutus.TxOut ->
   TxOut CtxUTxO era
 fromPlutusTxOut network out =
